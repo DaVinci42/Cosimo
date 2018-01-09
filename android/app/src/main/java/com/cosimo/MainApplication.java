b6b48c5bdbbe4ac85d25b6a7574d3a7e7b78ceb1
DaVinci42 <starrynight.ying@gmail.com>
2018-01-09 16:40:27 +0800

    add RNFS

--- android/app/src/main/java/com/cosimo/MainApplication.java
+++ android/app/src/main/java/com/cosimo/MainApplication.java
@@ -3,6 +3,7 @@ package com.cosimo;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import com.reactnative.photoview.PhotoViewPackage;
 import com.BV.LinearGradient.LinearGradientPackage;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -26,6 +27,7 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
             new PhotoViewPackage(),
             new LinearGradientPackage(),
             new VectorIconsPackage()
--- app/App.js
+++ app/App.js
@@ -6,4 +6,3 @@ export default class App extends Component {
 		return <AppDrawerNavigator />;
 	}
 }
-
